3a0279268f88c0fc788dd5d91ddb228555852f22
lika85456 <lika85456@post.cz>
2018-05-13 18:15:19 +0200

    update

---
+++ app/src/main/java/com/lika85456/industrio/MapRenderer.java
@@ -0,0 +1,10 @@
+package com.lika85456.industrio;
+
+import android.graphics.Canvas;
+
+public class MapRenderer extends Renderer {
+
+    public void render(Canvas canvas) {
+
+    }
+}
---
+++ app/src/main/java/com/lika85456/industrio/Renderer.java
@@ -0,0 +1,9 @@
+package com.lika85456.industrio;
+
+import android.graphics.Canvas;
+
+public class Renderer {
+    public void render(Canvas c) {
+
+    }
+}
